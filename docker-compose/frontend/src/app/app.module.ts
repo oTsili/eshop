05cae311bc727c8ec582bdb91ca9af8b5a9b75f7
otsili <o.tsilingeridis@gmail.com>
2022-06-09 15:44:03 +0000

    carousel, concept-sensence

--- docker-compose/frontend/src/app/app.module.ts
+++ docker-compose/frontend/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { AngularMaterialModule } from './modules/angular-material.module';
+// import { BootstrapModule } from './modules/bootstrap.module';
+import { NgbdCarouselPauseModule } from './modules/carousel-pause.module';
+import { ConceptSentenceComponent } from './shared/concept-sentence/concept-sentence.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ConceptSentenceComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    NgbdCarouselPauseModule,
+    // BootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ docker-compose/frontend/src/app/shared/concept-sentence/concept-sentence.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-concept-sentence',
+  templateUrl: './concept-sentence.component.html',
+  styleUrls: ['./concept-sentence.component.css']
+})
+export class ConceptSentenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
