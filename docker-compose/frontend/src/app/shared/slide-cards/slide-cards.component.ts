63ebcf1370d1fa79d74426db7692d22476dee5c2
otsili <o.tsilingeridis@gmail.com>
2022-06-10 00:20:14 +0300

    slide-cards

---
+++ docker-compose/frontend/src/app/shared/slide-cards/slide-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-card',
+  templateUrl: './slide-card.component.html',
+  styleUrls: ['./slide-card.component.css']
+})
+export class SlideCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
