3741a5ef6e2c8f367d70ae5705554c3bd27bf597
otsili <o.tsilingeridis@gmail.com>
2022-06-13 17:36:28 +0300

    arrows in click-drag-carousel

--- docker-compose/frontend/src/app/shared/click-drag-carousel/click-drag-carousel.component.ts
+++ docker-compose/frontend/src/app/shared/click-drag-carousel/click-drag-carousel.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { DragScrollComponent } from 'ngx-drag-scroll';
 
 @Component({
   selector: 'app-click-drag-carousel',
   templateUrl: './click-drag-carousel.component.html',
-  styleUrls: ['./click-drag-carousel.component.css'],
+  styleUrls: [
+    './click-drag-carousel.component.css',
+    './click-drag-carousel.component.scss',
+  ],
 })
 export class ClickDragCarouselComponent implements OnInit {
-  @ViewChild('nav') ds;
+  @ViewChild('nav', { read: DragScrollComponent }) ds: DragScrollComponent;
+  current_slide: number;
   products = [
     {
       src: './assets/images/1.jpeg',
       altSrc: '/assets/images/1-1.jpeg',
       name: 'Πέδιλο μπλε',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's ",
       price: '65,00',
       special_price: '35,00',
       sales: '25',
@@ -20,6 +27,7 @@ export class ClickDragCarouselComponent implements OnInit {
       src: './assets/images/2.jpeg',
       altSrc: '/assets/images/2-1.jpeg',
       name: 'Πέδιλο μπλε',
+      description: 'Δερμάτινη Τσάντα Ώμου με Tassels',
       price: '65,00',
       special_price: '35,00',
       sales: '25',
@@ -28,6 +36,7 @@ export class ClickDragCarouselComponent implements OnInit {
       src: './assets/images/3.jpeg',
       altSrc: '/assets/images/3-1.jpeg',
       name: 'Πέδιλο μπλε',
+      description: 'Δερμάτινη Τσάντα Ώμου με Tassels',
       price: '65,00',
       special_price: '35,00',
       sales: '25',
@@ -37,6 +46,7 @@ export class ClickDragCarouselComponent implements OnInit {
       altSrc: '/assets/images/4-1.jpeg',
       name: 'Πέδιλο μπλε',
       price: '65,00',
+      description: 'Δερμάτινη Τσάντα Ώμου με Tassels',
       special_price: '35,00',
       sales: '25',
     },
@@ -44,6 +54,7 @@ export class ClickDragCarouselComponent implements OnInit {
       src: './assets/images/5.jpeg',
       altSrc: '/assets/images/5-1.jpeg',
       name: 'Πέδιλο μπλε',
+      description: 'Δερμάτινη Τσάντα Ώμου με Tassels',
       price: '65,00',
       special_price: '35,00',
       sales: '25',
@@ -52,6 +63,7 @@ export class ClickDragCarouselComponent implements OnInit {
       src: './assets/images/6.jpeg',
       altSrc: '/assets/images/6-1.jpeg',
       name: 'Πέδιλο μπλε',
+      description: 'Δερμάτινη Τσάντα Ώμου με Tassels',
       price: '65,00',
       special_price: '35,00',
       sales: '25',
@@ -70,7 +82,13 @@ export class ClickDragCarouselComponent implements OnInit {
     this.ds.moveRight();
   }
 
-  moveTo(index) {
+  clickNext() {
+    let index = this.current_slide + 1;
+    this.ds.moveTo(index);
+  }
+
+  clickPrevious() {
+    let index = this.current_slide - 1;
     this.ds.moveTo(index);
   }
 }
