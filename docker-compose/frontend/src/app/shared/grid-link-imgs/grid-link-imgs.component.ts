b7dc12ffd8ef00408263b8fd4a97d6bb4641b722
otsili <o.tsilingeridis@gmail.com>
2022-06-14 11:46:00 +0300

    grid fix

--- docker-compose/frontend/src/app/shared/grid-link-imgs/grid-link-imgs.component.ts
+++ docker-compose/frontend/src/app/shared/grid-link-imgs/grid-link-imgs.component.ts
@@ -28,16 +28,6 @@ export class GridLinkImgsComponent implements OnInit {
       name: 'New Collection',
       imageSrc: './assets/images/categories/4.webp',
     },
-    {
-      id: '5',
-      name: 'Summer',
-      imageSrc: './assets/images/categories/5.webp',
-    },
-    {
-      id: '6',
-      name: 'All Weather',
-      imageSrc: './assets/images/categories/6.webp',
-    },
   ];
 
   constructor() {}
