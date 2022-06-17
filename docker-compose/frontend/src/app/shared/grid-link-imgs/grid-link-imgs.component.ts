5dbd4d7b7f1007b0ebf14652bf5a5d8cc17f986f
otsili <o.tsilingeridis@gmail.com>
2022-06-17 12:21:57 +0300

    grid lik imgs responsive

--- docker-compose/frontend/src/app/shared/grid-link-imgs/grid-link-imgs.component.ts
+++ docker-compose/frontend/src/app/shared/grid-link-imgs/grid-link-imgs.component.ts
@@ -9,22 +9,22 @@ import { ProductCategory } from './grid-link-imgs.interfaces';
 export class GridLinkImgsComponent implements OnInit {
   categories: ProductCategory[] = [
     {
-      id: '1',
+      id: '0',
       name: 'Winter',
       imageSrc: './assets/images/categories/1.webp',
     },
     {
-      id: '2',
+      id: '1',
       name: 'Summer',
       imageSrc: './assets/images/categories/2.webp',
     },
     {
-      id: '3',
+      id: '2',
       name: 'Kids',
       imageSrc: './assets/images/categories/3.webp',
     },
     {
-      id: '4',
+      id: '3',
       name: 'New Collection',
       imageSrc: './assets/images/categories/4.webp',
     },
@@ -32,5 +32,26 @@ export class GridLinkImgsComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.getCategoriesAreas(2);
+  }
+
+  getCategoriesAreas(colNum: number) {
+    let areas = '';
+    for (let [index, category] of this.categories.entries()) {
+      // console.log(category, index);
+      if (index === 0) {
+        areas = areas + `${index} `;
+      } else if (index === this.categories.length - 1) {
+        areas = areas + `${index}`;
+      } else if (index % colNum !== 0) {
+        areas = areas + `${index} |`;
+      } else {
+        areas = areas + ` ${index} `;
+      }
+    }
+
+    console.log(areas);
+    return areas;
+  }
 }
