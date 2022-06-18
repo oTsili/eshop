da7b7d2d3d8b364c8e596a781dcc26e6fe0cef7a
otsili <o.tsilingeridis@gmail.com>
2022-06-18 13:01:17 +0300

    custom carousel animation with css only

--- docker-compose/frontend/src/app/shared/custom-carousel/custom-carousel.component.ts
+++ docker-compose/frontend/src/app/shared/custom-carousel/custom-carousel.component.ts
@@ -1,27 +1,27 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Slide } from './custom-carousel.interface';
-import {
-  animate,
-  trigger,
-  transition,
-  useAnimation,
-  state,
-  style,
-} from '@angular/animations';
+// import { Slide } from './custom-carousel.interface';
+// import {
+//   animate,
+//   trigger,
+//   transition,
+//   useAnimation,
+//   state,
+//   style,
+// } from '@angular/animations';
 
-import {
-  AnimationType,
-  scaleIn,
-  scaleOut,
-  fadeIn,
-  fadeOut,
-  flipIn,
-  flipOut,
-  jackIn,
-  jackOut,
-  easein,
-  easeout,
-} from './custom-carousel.animations';
+// import {
+//   AnimationType,
+//   scaleIn,
+//   scaleOut,
+//   fadeIn,
+//   fadeOut,
+//   flipIn,
+//   flipOut,
+//   jackIn,
+//   jackOut,
+//   easein,
+//   easeout,
+// } from './custom-carousel.animations';
 import { Subscription } from 'rxjs';
 import { DynamicDatabase } from './dynamic-database';
 
@@ -29,50 +29,50 @@ import { DynamicDatabase } from './dynamic-database';
   selector: 'app-custom-carousel',
   templateUrl: './custom-carousel.component.html',
   styleUrls: ['./custom-carousel.component.scss'],
-  animations: [
-    trigger('slideAnimation', [
-      /* scale */
-      transition('void => scale', [
-        useAnimation(scaleIn, { params: { time: '500ms' } }),
-      ]),
-      transition('scale => void', [
-        useAnimation(scaleOut, { params: { time: '500ms' } }),
-      ]),
+  // animations: [
+  //   trigger('slideAnimation', [
+  //     /* scale */
+  //     transition('void => scale', [
+  //       useAnimation(scaleIn, { params: { time: '500ms' } }),
+  //     ]),
+  //     transition('scale => void', [
+  //       useAnimation(scaleOut, { params: { time: '500ms' } }),
+  //     ]),
 
-      /* fade */
-      transition('void => fade', [
-        useAnimation(fadeIn, { params: { time: '500ms' } }),
-      ]),
-      transition('fade => void', [
-        useAnimation(fadeOut, { params: { time: '500ms' } }),
-      ]),
-      /* flip */
-      transition('void => flip', [
-        useAnimation(flipIn, { params: { time: '500ms' } }),
-      ]),
-      transition('flip => void', [
-        useAnimation(flipOut, { params: { time: '500ms' } }),
-      ]),
-      /* easein */
-      transition('void => easein', [
-        useAnimation(easein, { params: { time: '500ms' } }),
-      ]),
-      transition('easein => void', [
-        useAnimation(easeout, { params: { time: '500ms' } }),
-      ]),
+  //     /* fade */
+  //     transition('void => fade', [
+  //       useAnimation(fadeIn, { params: { time: '500ms' } }),
+  //     ]),
+  //     transition('fade => void', [
+  //       useAnimation(fadeOut, { params: { time: '500ms' } }),
+  //     ]),
+  //     /* flip */
+  //     transition('void => flip', [
+  //       useAnimation(flipIn, { params: { time: '500ms' } }),
+  //     ]),
+  //     transition('flip => void', [
+  //       useAnimation(flipOut, { params: { time: '500ms' } }),
+  //     ]),
+  //     /* easein */
+  //     transition('void => easein', [
+  //       useAnimation(easein, { params: { time: '500ms' } }),
+  //     ]),
+  //     transition('easein => void', [
+  //       useAnimation(easeout, { params: { time: '500ms' } }),
+  //     ]),
 
-      /* JackInTheBox */
-      transition('void => jackInTheBox', [
-        useAnimation(jackIn, { params: { time: '700ms' } }),
-      ]),
-      transition('jackInTheBox => void', [
-        useAnimation(jackOut, { params: { time: '700ms' } }),
-      ]),
-    ]),
-  ],
+  //     /* JackInTheBox */
+  //     transition('void => jackInTheBox', [
+  //       useAnimation(jackIn, { params: { time: '700ms' } }),
+  //     ]),
+  //     transition('jackInTheBox => void', [
+  //       useAnimation(jackOut, { params: { time: '700ms' } }),
+  //     ]),
+  //   ]),
+  // ],
 })
 export class CustomCarouselComponent implements OnInit, OnDestroy {
-  animationType = 'easein';
+  // animationType = 'easein';
   imagesLengthSubscription: Subscription;
   intervalId: NodeJS.Timer | null;
 
