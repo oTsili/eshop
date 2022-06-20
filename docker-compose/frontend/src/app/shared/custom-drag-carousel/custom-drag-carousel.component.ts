88f073ae6797f1e7b76a0dfc2c796feedafb5a9d
otsili <o.tsilingeridis@gmail.com>
2022-06-20 14:34:04 +0300

    custom grab-carousel controls with animation

--- docker-compose/frontend/src/app/shared/custom-drag-carousel/custom-drag-carousel.component.ts
+++ docker-compose/frontend/src/app/shared/custom-drag-carousel/custom-drag-carousel.component.ts
@@ -20,13 +20,34 @@ export class CustomDragCarouselComponent implements OnInit, AfterViewInit {
   @ViewChild('slides') slides: ElementRef;
   pos = { top: 0, left: 0, x: 0, y: 0 };
   isMouseDown = false;
+  isAnimating = false;
+  _snapOffset = 0;
+  scrollToTimer: number | NodeJS.Timeout = -1;
+  snapAnimationFinished = new EventEmitter();
+  indexChanged = new EventEmitter();
+  _index = 0;
+  products = this.dynamicDatabase.products;
 
+  get snapOffset() {
+    return this._snapOffset;
+  }
+  set snapOffset(value) {
+    this._snapOffset = value;
+  }
+  get currIndex() {
+    return this._index;
+  }
+  set currIndex(value) {
+    if (value !== this._index) {
+      this._index = value;
+      this.indexChanged.emit(value);
+    }
+  }
   constructor(
     public dynamicDatabase: DynamicDatabase,
     private elementRef: ElementRef,
     private render: Renderer2
   ) {}
-  products = this.dynamicDatabase.products;
 
   ngOnInit(): void {
     // console.log(this.slide.nativeElement);
@@ -34,51 +55,101 @@ export class CustomDragCarouselComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     // console.log(this.wrapper.nativeElement);moveLeft
-    this.moveLeft();
   }
 
   moveLeft() {
-    const container = this.container.nativeElement;
-    const containerWidth = container.clientWidth;
-    const offsetWidth = container.offsetWidth;
+    let container = this.container.nativeElement;
+    let containerWidth = container.clientWidth;
+    let offsetWidth = container.offsetWidth;
 
-    const childrenObj = this.slides.nativeElement.children;
-    const childrenLen = Object.values(childrenObj).length;
-    this.wrapper.nativeElement.scrollLeft =
-      this.wrapper.nativeElement.scrollLeft -
-      (containerWidth + offsetWidth) / childrenLen;
-
-    console.log(this.wrapper.nativeElement.scrollLeft);
-    console.log((containerWidth + offsetWidth) / childrenLen);
-    console.log(this.wrapper.nativeElement);
-    // wrapper.scrollToTimer;
-  }
-
-  maximumIndex(containerWidth, childrenElements) {}
-
-  isScrollReachesRightEnd() {
-    const container = this.container.nativeElement;
-    const scrollLeftPos = container.scrollLeft + container.offsetWidth;
-    return scrollLeftPos >= container.scrollWidth;
+    let childrenObj = this.slides.nativeElement.children;
+    let childrenLen = Object.values(childrenObj).length;
+    this.scrollTo(
+      this.wrapper.nativeElement,
+      500,
+      containerWidth,
+      offsetWidth,
+      childrenLen,
+      'left'
+    );
   }
 
   moveRight() {
-    const container = this.container.nativeElement;
-    const containerWidth = container.clientWidth;
-    const offsetWidth = container.offsetWidth;
+    let container = this.container.nativeElement;
+    let containerWidth = container.clientWidth;
+    let offsetWidth = container.offsetWidth;
 
-    const childrenObj = this.slides.nativeElement.children;
-    const childrenLen = Object.values(childrenObj).length;
-    this.wrapper.nativeElement.scrollLeft =
-      this.wrapper.nativeElement.scrollLeft +
-      (containerWidth + offsetWidth) / childrenLen;
+    let childrenObj = this.slides.nativeElement.children;
+    let childrenLen = Object.values(childrenObj).length;
 
-    console.log(this.wrapper.nativeElement.scrollLeft);
-    console.log((containerWidth + offsetWidth) / childrenLen);
-    console.log(this.wrapper.nativeElement);
+    this.scrollTo(
+      this.wrapper.nativeElement,
+      500,
+      containerWidth,
+      offsetWidth,
+      childrenLen,
+      'right'
+    );
   }
 
-  scrollTo(element, to, duration) {}
+  scrollTo(
+    element: HTMLElement,
+    duration: number,
+    containerWidth: number,
+    offsetWidth: number,
+    childrenLen: number,
+    direction: string
+  ) {
+    const self = this;
+    self.isAnimating = true;
+    let start, change, increment;
+    if (direction == 'right') {
+      start = element.scrollLeft;
+      change =
+        (element.scrollLeft +
+          (containerWidth + offsetWidth) / childrenLen -
+          start) *
+        1.9;
+      increment = 20;
+    } else if (direction === 'left') {
+      start = element.scrollLeft;
+      change =
+        (element.scrollLeft -
+          (containerWidth + offsetWidth) / childrenLen -
+          start) *
+        1.9;
+      increment = 20;
+    }
+
+    let currentTime = 0;
+    // t = current time
+    // b = start value
+    // c = change in value
+    // d = duration
+    const easeInOutQuad = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) {
+        return (c / 2) * t * t + b;
+      }
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+    const animateScroll = function () {
+      currentTime += increment;
+      element.scrollLeft = easeInOutQuad(currentTime, start, change, duration);
+
+      if (currentTime < duration) {
+        self.scrollToTimer = setTimeout(animateScroll, increment);
+      } else {
+        // run one more frame to make sure the animation is fully finished
+        setTimeout(() => {
+          self.isAnimating = false;
+          self.snapAnimationFinished.emit(self.currIndex);
+        }, increment);
+      }
+    };
+    animateScroll();
+  }
 
   mouseDownHandler(e: MouseEvent) {
     e.preventDefault();
