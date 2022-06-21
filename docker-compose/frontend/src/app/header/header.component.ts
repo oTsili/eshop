1c0c06e29ce18bc5a6cb8707d2b6f6d5eda4bcb4
otsili <o.tsilingeridis@gmail.com>
2022-06-21 10:58:56 +0300

    header-component search button

--- docker-compose/frontend/src/app/header/header.component.ts
+++ docker-compose/frontend/src/app/header/header.component.ts
@@ -8,10 +8,16 @@ import { navBarElement } from './header.interfaces';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isActiveClassEnabled = false;
   initialData: navBarElement[];
   constructor(private database: DynamicDatabase) {
     this.initialData = this.database.navBarElements;
   }
 
   ngOnInit(): void {}
+
+  toggleActiveClass() {
+    this.isActiveClassEnabled = !this.isActiveClassEnabled;
+    console.log(this.isActiveClassEnabled);
+  }
 }
