48e39f8d959b18ed5094101a2d8ed378b6517527
otsili <o.tsilingeridis@gmail.com>
2022-06-21 19:29:43 +0300

    search page header style

--- docker-compose/frontend/src/app/user/search/dynamic-database.ts
+++ docker-compose/frontend/src/app/user/search/dynamic-database.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class DynamicDatabase {
   searchPage = {
-    header: 'Αποτελέσματα αναζήτησης',
+    header: 'Αποτελέσματα αναζήτησης για :',
   };
 }
