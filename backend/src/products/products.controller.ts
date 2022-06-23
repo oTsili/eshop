dccb3494d290ee0336b192e851bd9e759dfa442e
otsili <o.tsilingeridis@gmail.com>
2022-06-23 08:51:03 +0300

    MEAN stack ready

---
+++ backend/src/products/products.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { response } from 'express';
+import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Post()
+  async createProduct(@Res() response, @Body() product: Product) {
+    const newProduct = await this.productService.create(product);
+    return response.status(HttpStatus.CREATED).json({
+      newProduct,
+    });
+  }
+
+  @Get('')
+  async fetchAll(@Res() response) {
+    const products = await this.productService.findAll();
+    return response.status(HttpStatus.OK).json({ products });
+  }
+}
--- k8s/video-chat-test/node_modules/@otmilms/common/build/middlewares/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-var catchAsync = function (fn) {
-    return function (req, res, next) {
-        fn(req, res, next).catch(next);
-    };
-};
-exports.catchAsync = catchAsync;
