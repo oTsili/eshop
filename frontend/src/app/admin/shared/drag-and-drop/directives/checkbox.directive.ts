51124caa03417bd48ab0f677eea900a277594e5d
otsili <o.tsilingeridis@gmail.com>
2022-08-20 14:03:21 +0300

    radio button directive for main alt cover page

---
+++ frontend/src/app/admin/shared/drag-and-drop/directives/checkbox.directive.ts
@@ -0,0 +1,82 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({ selector: '[checkbox]' })
+export class CheckboxDirective implements OnInit {
+  element: HTMLElement;
+
+  mainInputSiblings: HTMLElement[];
+  altInputSiblings: HTMLElement[];
+  divSiblings: HTMLElement[];
+  inputSiblings: HTMLElement[];
+  input: HTMLElement;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.element = this.elementRef.nativeElement;
+
+    this.input = this.elementRef.nativeElement.querySelector('input');
+
+    this.mainInputSiblings =
+      this.elementRef.nativeElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelectorAll(
+        '.main-radio, .main-radio > input'
+      );
+
+    this.altInputSiblings =
+      this.elementRef.nativeElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelectorAll(
+        '.alt-radio, .alt-radio > input'
+      );
+
+    this.divSiblings =
+      this.elementRef.nativeElement.parentElement.parentElement.querySelectorAll(
+        '.checkbox'
+      );
+
+    this.inputSiblings =
+      this.elementRef.nativeElement.parentElement.parentElement.querySelectorAll(
+        'input'
+      );
+  }
+
+  @HostListener('click', ['$event'])
+  activateCheck(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log(this.altInputSiblings);
+    console.log(this.mainInputSiblings);
+    console.log(this.divSiblings);
+    console.log(this.inputSiblings);
+
+    // remove the all the main or alt radio buttons,
+    // respectivly to what is the current clicked button
+    if (this.element.classList.contains('main-radio')) {
+      console.log(this.element);
+      this.mainInputSiblings.forEach((el) => {
+        this.renderer.removeClass(el, 'checked');
+        this.renderer.removeAttribute(el, 'checked');
+      });
+    } else if (this.element.classList.contains('alt-radio')) {
+      this.altInputSiblings.forEach((el) => {
+        this.renderer.removeClass(el, 'checked');
+        this.renderer.removeAttribute(el, 'checked');
+      });
+    }
+
+    // remove from the current level the checked class and attribute
+    this.divSiblings.forEach((el) => {
+      this.renderer.removeClass(el, 'checked');
+    });
+    this.inputSiblings.forEach((el) => {
+      this.renderer.removeAttribute(el, 'checked');
+    });
+    // add the class checked
+    this.renderer.addClass(this.element, 'checked');
+    this.renderer.setAttribute(this.input, 'checked', '');
+  }
+}
--- frontend/src/app/admin/shared/drag-and-drop/single-file/single-file.module.ts
+++ frontend/src/app/admin/shared/drag-and-drop/single-file/single-file.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleFileComponent } from './single-file.component';
 import { ProgressBarModule } from '../../progress-bar/progress-bar.module';
+import { CheckboxDirective } from '../directives/checkbox.directive';
 
 
 
 @NgModule({
-  declarations: [SingleFileComponent],
-  imports: [CommonModule, ProgressBarModule],exports:[SingleFileComponent]
+  declarations: [SingleFileComponent, CheckboxDirective],
+  imports: [CommonModule, ProgressBarModule],
+  exports: [SingleFileComponent],
 })
 export class SingleFileModule {}
