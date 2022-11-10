24c44d8db2e495c74b5b4021abe7ff3f5612fce5
otsili <o.tsilingeridis@gmail.com>
2022-11-10 20:07:36 +0200

    edit-product description

--- frontend/src/app/user/shared/product-showcase/side-bar/side-bar.component.ts
+++ frontend/src/app/user/shared/product-showcase/side-bar/side-bar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Product } from 'src/app/user/product/product.interface';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent implements OnInit, OnChanges {
+  @Input() product: Product;
+  oldPrice: number;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.product = changes['product'].currentValue;
+    console.log(this.product);
 
-  ngOnInit(): void {
+    // compute the pre-sales(old) price from the sales percentage
+    this.oldPrice =
+      parseInt(this.product.price) +
+      parseInt(this.product.price) * (parseInt(this.product.sales) / 100);
   }
 
+  hasSales(sales: string) {
+    return parseInt(sales) > 0;
+  }
 }
