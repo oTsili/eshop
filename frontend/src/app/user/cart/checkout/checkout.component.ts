c75a3a187c600b6b57dea80d4f20b8e35ad889c4
otsili <o.tsilingeridis@gmail.com>
2022-11-14 22:41:30 +0200

    checkout

--- frontend/src/app/user/cart/checkout/checkout.component.ts
+++ frontend/src/app/user/cart/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Breadcrumb } from '../../shared/breadcrumb/breadcrumb.interfaces';
+import { BreadcrumbService } from '../../shared/breadcrumb/breadcrumb.service';
 import { UserAppService } from '../../user-app.service';
 
 @Component({
@@ -7,9 +10,31 @@ import { UserAppService } from '../../user-app.service';
   styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-  constructor(private userAppService: UserAppService) {}
+  pageHeader: string;
+  breadcrumbItems: Breadcrumb[];
+  constructor(
+    private userAppService: UserAppService,
+    private router: Router,
+    private breadcrumbService: BreadcrumbService
+  ) {}
 
   ngOnInit(): void {
     this.userAppService.onDisableHeaderAndFooter(true);
+    this.initializeBreadcrumbs();
+  }
+
+  initializeBreadcrumbs() {
+    // get each intermediate route from the url
+    let routes = this.router.url.split('/');
+    // delete the first (which is an empty string)
+    routes.shift();
+    // ths last route becomes the page header
+    this.pageHeader = routes[routes.length - 1];
+
+    // get an array of Breadcrumb items from the routes above
+    this.breadcrumbItems = this.breadcrumbService.getBreadcrumbs(
+      routes,
+      this.router.url
+    );
   }
 }
--- frontend/src/app/user/cart/checkout/checkout.module.ts
+++ frontend/src/app/user/cart/checkout/checkout.module.ts
@@ -1,10 +1,26 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { BreadcrumbModule } from '../../shared/breadcrumb/breadcrumb.module';
+import { HttpLoaderFactory } from '../../shared/loader-factory';
 import { CheckoutComponent } from './checkout.component';
 
 @NgModule({
   declarations: [CheckoutComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BreadcrumbModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   exports: [CheckoutComponent],
 })
 export class CheckoutModule {}
